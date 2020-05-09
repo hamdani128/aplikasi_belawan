74fd31e5023ec6a2d8037daa1dc65bb7b982d190
unknown <hamdaniversi08@gmail.com>
2020-05-09 12:11:18 +0700

    Api1

--- routes/api.php
+++ routes/api.php
@@ -35,6 +35,6 @@ Route::get('dayphg', function(){
 });
 
 Route::get('allphg', function(){
-    return response()->json(TransactionSmart::all());
+    return response()->json(TransactionPhgt::all());
 });
 
