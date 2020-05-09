6a49da99d93566879d53ade14f0fa8060f1737aa
unknown <hamdaniversi08@gmail.com>
2020-05-09 12:06:14 +0700

    Api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\TransactionPhgt;
+use App\Models\TransactionSmart;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('daysmart', function(){
+        $data = TransactionSmart::get()->where('tanggal', date('2020-05-09'));
+        return response()->json($data);
+});
+
+Route::get('allsmart', function(){
+    return response()->json(TransactionSmart::all());
+});
+
+Route::get('dayphg', function(){
+    $data =TransactionPhgt::where('tanggal', date('Y-m-d'));
+    return response()->json($data);
+});
+
+Route::get('allphg', function(){
+    return response()->json(TransactionSmart::all());
+});
+
