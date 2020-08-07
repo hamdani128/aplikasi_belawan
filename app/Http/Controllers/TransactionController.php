c89449dbb7c7abb1953f5f482ef9ab6bee6b82f5
unknown <hamdaniversi08@gmail.com>
2020-08-07 23:33:18 +0700

    07/08/2020 23:33

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -45,34 +45,34 @@ class TransactionController extends Controller
             // $kemarin =date('Y-m-d', strtotime("-1 day", strtotime(date('Y-m-d'))));
             // $hari = date('Y-m-d').' 19:00:00';
                 $trphg = transactionPhgt::where('user_id','3')
-                        ->where('created_at', '>' ,date('Y-m-d').' 19:00:00')
-                        ->where('created_at', '<' ,date('Y-m-d').' 00:00:00')
+                        ->where('created_at', '=>' ,date('Y-m-d').' 19:00:00')
+                        ->where('created_at', '=<' ,date('Y-m-d').' 00:00:00')
                         ->orWhere('user_id','3')
-                        ->where('created_at', '>' ,date('Y-m-d').' 00:00:00')
-                        ->where('created_at', '<' ,date('Y-m-d').' 07:00:00')
+                        ->where('created_at', '=>' ,date('Y-m-d').' 00:00:00')
+                        ->where('created_at', '=<' ,date('Y-m-d').' 07:00:00')
                         ->get();
          
             $trsmart = TransactionSmart::where('user_id','3')
-                        ->where('created_at', '>' ,date('Y-m-d').' 19:00:00')
-                        ->where('created_at', '<' ,date('Y-m-d').' 00:00:00')
+                        ->where('created_at', '=>' ,date('Y-m-d').' 19:00:00')
+                        ->where('created_at', '=<' ,date('Y-m-d').' 00:00:00')
                         ->orWhere('user_id','3')
-                        ->where('created_at', '>' ,date('Y-m-d').' 00:00:00')
-                        ->where('created_at', '<' ,date('Y-m-d').' 07:00:00')
+                        ->where('created_at', '=>' ,date('Y-m-d').' 00:00:00')
+                        ->where('created_at', '=<' ,date('Y-m-d').' 07:00:00')
                         ->get();
 
             $trukphg = TransactionPhgt::where('user_id','3')
-                        ->where('created_at', '>' ,date('Y-m-d').' 19:00:00')
-                        ->where('created_at', '<' ,date('Y-m-d').' 00:00:00')
+                        ->where('created_at', '=>' ,date('Y-m-d').' 19:00:00')
+                        ->where('created_at', '=<' ,date('Y-m-d').' 00:00:00')
                         ->orWhere('user_id','3')
-                        ->where('created_at', '>' ,date('Y-m-d').' 00:00:00')
-                        ->where('created_at', '<' ,date('Y-m-d').' 07:00:00')
+                        ->where('created_at', '=>' ,date('Y-m-d').' 00:00:00')
+                        ->where('created_at', '=<' ,date('Y-m-d').' 07:00:00')
                         ->count();
             $truksmart = TransactionSmart::where('user_id','3')
-                        ->where('created_at', '>' ,date('Y-m-d').' 19:00:00')
-                        ->where('created_at', '<' ,date('Y-m-d').' 00:00:00')
+                        ->where('created_at', '=>' ,date('Y-m-d').' 19:00:00')
+                        ->where('created_at', '=<' ,date('Y-m-d').' 00:00:00')
                         ->orWhere('user_id','3')
-                        ->where('created_at', '>' ,date('Y-m-d').' 00:00:00')
-                        ->where('created_at', '<' ,date('Y-m-d').' 07:00:00')
+                        ->where('created_at', '=>' ,date('Y-m-d').' 00:00:00')
+                        ->where('created_at', '=<' ,date('Y-m-d').' 07:00:00')
                         ->count();
         }
         // return dd($trphg);
